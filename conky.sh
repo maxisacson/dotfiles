1c91c5cf558bbd07f252d7a5031f5a7cfcac909d
Max Isacson <max.isacson@physics.uu.se>
2019-07-02 10:52:37 +0200

    fixed conky.sh

--- conky.sh
+++ conky.sh
@@ -3,7 +3,7 @@
 n_monitors=$(xrandr -q | grep " connected" | wc -l)
 
 if [[ $n_monitors -eq 2 ]]; then
-    conky --config=~/.conky/conkyrc-dualdisplay
+    conky --config=$HOME/.conky/conkyrc-dualdisplay
 elif [[ $n_monitors -eq 1 ]]; then
-    conky --config=~/.conky/conkyrc-singledisplay
+    conky --config=$HOME/.conky/conkyrc-singledisplay
 fi
