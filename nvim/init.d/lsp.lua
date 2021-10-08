e30e5ca1fd57b84d31914c24fd30ecef6741a5be
Max Isacson <5937020+maxisacson@users.noreply.github.com>
2021-10-08 17:59:44 +0200

    better separation between lsp and nvim-cmp configs

--- nvim/init.d/lsp.lua
+++ nvim/init.d/lsp.lua
@@ -1,3 +1,4 @@
+vim.lsp.set_log_level('trace')
 local nvim_lsp = require('lspconfig')
 
 local on_attach = function(client, bufnr)
@@ -70,18 +71,14 @@ vim.lsp.handlers['textDocument/publishDiagnostics'] = vim.lsp.with(
 
 nvim_lsp.pylsp.setup {
     on_attach = on_attach,
-    capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
 }
 
 nvim_lsp.cmake.setup {
     on_attach = on_attach,
-    capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
 }
 
 nvim_lsp.clangd.setup {
     on_attach = on_attach,
-    capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities()),
-
     cmd = {"clangd", "--background-index", "--compile-commands-dir=build","--clang-tidy",
         "--clang-tidy-checks='"..
         "-*,"..
