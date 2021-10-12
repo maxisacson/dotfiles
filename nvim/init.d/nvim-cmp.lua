9a7c212f7bb1af335d354b4a4d1a259c4228309b
Max Isacson <5937020+maxisacson@users.noreply.github.com>
2021-10-12 21:38:47 +0200

    update tab-bindings in nvim-cmp

--- nvim/init.d/nvim-cmp.lua
+++ nvim/init.d/nvim-cmp.lua
@@ -12,8 +12,27 @@ cmp.setup({
         ['<C-u>'] = cmp.mapping.scroll_docs(-4),
         ['<C-e>'] = cmp.mapping.close(),
         ['<CR>'] = cmp.mapping.confirm({ select = false }),
-        ['<Tab>'] = cmp.mapping(cmp.mapping.select_next_item(), { 'i', 's' }),
-        ['<S-Tab>'] = cmp.mapping(cmp.mapping.select_prev_item(), { 'i', 's' })
+
+        ["<Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+                cmp.select_next_item()
+            elseif vim.fn["vsnip#available"]() == 1 then
+                feedkey("<Plug>(vsnip-expand-or-jump)", "")
+            else
+                fallback()
+            end
+        end, { "i", "s" }),
+
+        ["<S-Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+                cmp.select_prev_item()
+            elseif vim.fn["vsnip#jumpable"](-1) == 1 then
+                feedkey("<Plug>(vsnip-jump-prev)", "")
+            else
+                fallback()
+            end
+        end, { "i", "s" }),
+
     },
     sources = {
         { name = 'nvim_lsp' },
