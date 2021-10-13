082581f927c718026caf733f5d9a408d95a50b8c
Max Isacson <5937020+maxisacson@users.noreply.github.com>
2021-10-13 18:24:57 +0200

    use git_files in git repos

--- nvim/init.d/telescope.lua
+++ nvim/init.d/telescope.lua
@@ -5,8 +5,14 @@ vim.api.nvim_set_keymap('n', '<leader>fg', '<cmd>lua require("telescope.builtin"
 vim.api.nvim_set_keymap('n', '<leader>fb', '<cmd>lua require("telescope.builtin").buffers()<CR>',    {noremap=true, silent=true})
 vim.api.nvim_set_keymap('n', '<leader>fh', '<cmd>lua require("telescope.builtin").help_tags()<CR>',  {noremap=true, silent=true})
 
-vim.api.nvim_set_keymap('n', '<C-Space>', '<cmd>lua require("telescope.builtin").find_files()<CR>', {noremap=true, silent=true})
+vim.api.nvim_set_keymap('n', '<C-Space>', '<cmd>lua telescope_project_files()<CR>', {noremap=true, silent=true})
 vim.api.nvim_set_keymap('n', '<C-\\>', '<cmd>lua require("telescope.builtin").live_grep()<CR>',  {noremap=true, silent=true})
+
+_G.telescope_project_files = function()
+  local opts = {} -- define here if you want to define something
+  local ok = pcall(require'telescope.builtin'.git_files, opts)
+  if not ok then require'telescope.builtin'.find_files(opts) end
+end
 
 local actions = require'telescope.actions'
 require'telescope'.setup{
