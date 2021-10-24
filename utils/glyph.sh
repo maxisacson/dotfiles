79cc8a7de277c970b8797eecdd299943882aaef6
Max Isacson <5937020+maxisacson@users.noreply.github.com>
2021-10-24 21:29:20 +0200

    updated glyph.sh

--- utils/glyph.sh
+++ utils/glyph.sh
@@ -1,4 +1,13 @@
 #!/usr/bash
 
 fc-list ":charset=$1" | awk -F: '{print $2}' | sort | uniq | awk '{sub(/\s+/,""); printf "%-3d %s\n",NR,$0}'
-echo -e "\ncontaining glyph: \U$1"
+
+eval "c=$'\U$1'"
+echo -e "\ncontaining glyph: $c (U+$1)"
+
+# this is not reliable
+# font=$(FC_DEBUG=4 pango-view --font=monospace -t $c -q 2>&1 | grep family: | tail -n1 | awk 'match($0,/^\s*(family:)\s*"(.*)".*$/,m) {print m[2]}')
+# echo "rendered with (best guess): $font"
+
+# uncomment this to show what font is used to render the glyph
+# FC_DEBUG=4 pango-view --font=monospace -t $c -q | grep family:
