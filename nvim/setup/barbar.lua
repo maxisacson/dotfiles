dd70b354b2a2e491a348510b954d73ed73517da2
Max Isacson <5937020+maxisacson@users.noreply.github.com>
2022-04-20 21:46:41 +0200

    updated barbar config to lua

--- nvim/setup/barbar.lua
+++ nvim/setup/barbar.lua
@@ -1,112 +1,117 @@
-vim.cmd([[
-    " Move to previous/next
-    nnoremap <silent>    <Leader>w :BufferPrevious<CR>
-    nnoremap <silent>    <Leader>e :BufferNext<CR>
-    " Re-order to previous/next
-    nnoremap <silent>    <Leader>s :BufferMovePrevious<CR>
-    nnoremap <silent>    <Leader>d :BufferMoveNext<CR>
-    " Goto buffer in position...
-    nnoremap <silent>    <Leader>1 :BufferGoto 1<CR>
-    nnoremap <silent>    <Leader>2 :BufferGoto 2<CR>
-    nnoremap <silent>    <Leader>3 :BufferGoto 3<CR>
-    nnoremap <silent>    <Leader>4 :BufferGoto 4<CR>
-    nnoremap <silent>    <Leader>5 :BufferGoto 5<CR>
-    nnoremap <silent>    <Leader>6 :BufferGoto 6<CR>
-    nnoremap <silent>    <Leader>7 :BufferGoto 7<CR>
-    nnoremap <silent>    <Leader>8 :BufferGoto 8<CR>
-    nnoremap <silent>    <Leader>9 :BufferLast<CR>
-    " Close buffer
-    nnoremap <silent>    <Leader>qq :BufferClose<CR>
-    " Wipeout buffer
-    "                          :BufferWipeout<CR>
-    " Close commands
-    "                          :BufferCloseAllButCurrent<CR>
-    "                          :BufferCloseBuffersLeft<CR>
-    "                          :BufferCloseBuffersRight<CR>
-    " Magic buffer-picking mode
-    nnoremap <silent> <C-s>    :BufferPick<CR>
-    " Sort automatically by...
-    nnoremap <silent> <Space>bd :BufferOrderByDirectory<CR>
-    nnoremap <silent> <Space>bl :BufferOrderByLanguage<CR>
+local map = vim.keymap.set
+local opts = { silent = true }
 
-    " Other:
-    " :BarbarEnable - enables barbar (enabled by default)
-    " :BarbarDisable - very bad command, should never be used
+-- Move to previous/next
+map('n', '<Leader>w', ':BufferPrevious<CR>', opts)
+map('n', '<Leader>e', ':BufferNext<CR>', opts)
+-- Re-order to previous/next
+map('n', '<Leader>s', ':BufferMovePrevious<CR>', opts)
+map('n', '<Leader>d', ':BufferMoveNext<CR>', opts)
+-- Goto buffer in position...
+map('n', '<Leader>1', ':BufferGoto 1<CR>', opts)
+map('n', '<Leader>2', ':BufferGoto 2<CR>', opts)
+map('n', '<Leader>3', ':BufferGoto 3<CR>', opts)
+map('n', '<Leader>4', ':BufferGoto 4<CR>', opts)
+map('n', '<Leader>5', ':BufferGoto 5<CR>', opts)
+map('n', '<Leader>6', ':BufferGoto 6<CR>', opts)
+map('n', '<Leader>7', ':BufferGoto 7<CR>', opts)
+map('n', '<Leader>8', ':BufferGoto 8<CR>', opts)
+map('n', '<Leader>9', ':BufferLast<CR>', opts)
+-- Close buffer
+map('n', '<Leader>qq', ':BufferClose<CR>', opts)
+-- Wipeout buffer
+--                          :BufferWipeout<CR>
+-- Close commands
+--                          :BufferCloseAllButCurrent<CR>
+--                          :BufferCloseBuffersLeft<CR>
+--                          :BufferCloseBuffersRight<CR>
+-- Magic buffer-picking mode
+map('n', '<C-s>', ':BufferPick<CR>', opts)
+-- Sort automatically by...
+map('n', '<Space>bd', ':BufferOrderByDirectory<CR>', opts)
+map('n', '<Space>bl', ':BufferOrderByLanguage<CR>', opts)
 
-    let bufferline = get(g:, 'bufferline', {})
-    let bufferline.animation = v:false " disable/enable animation
-    let bufferline.closable = v:false " disable/enable close button
-    let bufferline.clickable = v:false " disable/enable clickable tabs
-    let bufferline.icons = 'both'
-    let bufferline.icon_custom_colors = v:false
-    let bufferline.maximum_padding = 0
-    " let bufferline.icon_separator_active = ''
-    " let bufferline.icon_separator_inactive = ''
+-- Other:
+-- :BarbarEnable - enables barbar (enabled by default)
+-- :BarbarDisable - very bad command, should never be used
 
+vim.g.bufferline = {
+    animation = false, -- disable/enable animation
+    closable = false, -- disable/enable close button
+    clickable = false, -- disable/enable clickable tabs
+    icons = 'both',
+    icon_custom_colors = false,
+    maximum_padding = 0
+    -- icon_separator_active = ''
+    -- icon_separator_inactive = ''
+}
 
-    function! s:fg(group)
-        let hl = nvim_get_hl_by_name(a:group, 1)
-        return printf("#%06x", hl.foreground)
-    endfunction
+local function fg(group)
+    local hl = vim.api.nvim_get_hl_by_name(group, 1)
+    return string.format('#%06x', hl.foreground)
+end
 
-    let s:bg0 = s:fg("GruvboxBg0")
-    let s:bg1 = s:fg("GruvboxBg1")
-    let s:bg2 = s:fg("GruvboxBg2")
-    let s:bg4 = s:fg("GruvboxBg4")
-    let s:fg1 = s:fg("GruvboxFg1")
-    let s:fg4 = s:fg("GruvboxFg4")
-    let s:red = s:fg("GruvboxRed")
-    let s:orange = s:fg("GruvboxOrange")
-    let s:blue = s:fg("GruvboxBlue")
-    let s:yellow  = s:fg("GruvboxYellow")
-    let s:purple = s:fg("GruvboxPurple")
-    let s:aqua = s:fg("GruvboxAqua")
-    let s:green = s:fg("GruvboxGreen")
-    let s:gray = s:fg("GruvboxGray")
+local bg0 = fg("GruvboxBg0")
+local bg1 = fg("GruvboxBg1")
+local bg2 = fg("GruvboxBg2")
+local bg4 = fg("GruvboxBg4")
+local fg1 = fg("GruvboxFg1")
+local fg4 = fg("GruvboxFg4")
+local red = fg("GruvboxRed")
+local orange = fg("GruvboxOrange")
+local blue = fg("GruvboxBlue")
+local yellow  = fg("GruvboxYellow")
+local purple = fg("GruvboxPurple")
+local aqua = fg("GruvboxAqua")
+local green = fg("GruvboxGreen")
+local gray = fg("GruvboxGray")
 
-    " Meaning of terms:
-    "
-    " format: "Buffer" + status + part
-    "
-    " status:
-    "     *Current: current buffer
-    "     *Visible: visible but not current buffer
-    "    *Inactive: invisible but not current buffer
-    "
-    " part:
-    "        *Icon: filetype icon
-    "       *Index: buffer index
-    "         *Mod: when modified
-    "        *Sign: the separator between buffers
-    "      *Target: letter in buffer-picking mode
-    "
-    " BufferTabpages: tabpage indicator
-    " BufferTabpageFill: filler after the buffer section
-    " BufferOffset: offset section, created with set_offset()
+-- Meaning of terms:
+--
+-- format: "Buffer" + status + part
+--
+-- status:
+--     *Current: current buffer
+--     *Visible: visible but not current buffer
+--    *Inactive: invisible but not current buffer
+--
+-- part:
+--        *Icon: filetype icon
+--       *Index: buffer index
+--         *Mod: when modified
+--        *Sign: the separator between buffers
+--      *Target: letter in buffer-picking mode
+--
+-- BufferTabpages: tabpage indicator
+-- BufferTabpageFill: filler after the buffer section
+-- BufferOffset: offset section, created with set_offset()
 
-    execute "hi BufferCurrent gui=bold guifg=" . s:fg1 . " guibg=" . s:bg2
-    execute "hi BufferCurrentIndex guifg=" . s:fg4 . " guibg=" . s:bg2
-    execute "hi BufferCurrentMod gui=bold guifg=" . s:yellow . " guibg=" . s:bg2
-    execute "hi BufferCurrentSign guifg=" . s:bg2 . " guibg=" . s:bg2
-    execute "hi BufferCurrentTarget gui=bold guifg=" . s:red . " guibg=" . s:bg2
+local function hi(...)
+    return vim.api.nvim_set_hl(0, ...)
+end
 
-    execute "hi BufferVisible guifg=" . s:blue . " guibg=" . s:bg1
-    execute "hi BufferVisibleIndex guifg=" . s:blue . " guibg=" . s:bg1
-    execute "hi BufferVisibleMod guifg=" . s:yellow . " guibg=" . s:bg1
-    execute "hi BufferVisibleSign guifg=" . s:bg2 . " guibg=" . s:bg1
-    execute "hi BufferVisibleTarget gui=bold guifg=" . s:red . " guibg=" . s:bg1
+hi('BufferCurrent', { fg=fg1, bg=bg2, bold=1 })
+hi('BufferCurrentIndex', { fg=fg4, bg=bg2 })
+hi('BufferCurrentMod', { fg=yellow, bg=bg2, bold=1 })
+hi('BufferCurrentSign', { fg=bg2, bg=bg2 })
+hi('BufferCurrentTarget', { fg=red, bg=bg2, bold=1 })
 
-    execute "hi BufferInactive guifg=" . s:bg4 . " guibg=" . s:bg1
-    execute "hi BufferInactiveIndex guifg=" . s:bg4 . " guibg=" . s:bg1
-    execute "hi BufferInactiveMod guifg=" . s:yellow . " guibg=" . s:bg1
-    execute "hi BufferInactiveSign guifg=" . s:bg2 . " guibg=" . s:bg1
-    execute "hi BufferInactiveTarget gui=bold guifg=" . s:red . " guibg=" . s:bg1
+hi('BufferVisible', { fg=blue, bg=bg1 })
+hi('BufferVisibleIndex', { fg=blue, bg=bg1 })
+hi('BufferVisibleMod', { fg=yellow, bg=bg1 })
+hi('BufferVisibleSign', { fg=bg2, bg=bg1 })
+hi('BufferVisibleTarget', { fg=red, bg=bg1, bold=1 })
 
-    execute "hi BufferTabpages gui=bold guifg=" . s:orange . " guibg=" . s:bg1
-    execute "hi BufferTabpageFill guifg=" . s:bg4 . " guibg=" . s:bg1
+hi('BufferInactive', { fg=bg4, bg=bg1 })
+hi('BufferInactiveIndex', { fg=bg4, bg=bg1 })
+hi('BufferInactiveMod', { fg=yellow, bg=bg1 })
+hi('BufferInactiveSign', { fg=bg2, bg=bg1 })
+hi('BufferInactiveTarget', { fg=red, bg=bg1, bold=1 })
 
-    execute "hi link BufferCurrentIcon  BufferCurrent"
-    execute "hi link BufferVisibleIcon BufferVisible"
-    execute "hi link BufferInactiveIcon BufferInactive"
-    execute "hi link BufferOffset BufferTabpageFill"
-]])
+hi('BufferTabpages', { fg=orange, bg=bg1, bold=1 })
+hi('BufferTabpageFill', { fg=bg4, bg=bg1 })
+
+hi('BufferCurrentIcon', { link='BufferCurrent' })
+hi('BufferVisibleIcon', { link='BufferVisible' })
+hi('BufferInactiveIcon', { link='BufferInactive' })
+hi('BufferOffset', { link='BufferTabpageFill' })
