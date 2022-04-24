ca6c16d2d0ac46e125c64dba8137557ab8bea96f
Max Isacson <5937020+maxisacson@users.noreply.github.com>
2022-04-24 13:36:49 +0200

    port clang-format config to lua

--- nvim/setup/clang-format.lua
+++ nvim/setup/clang-format.lua
@@ -1,17 +1,20 @@
 -- vim-clang-format config
 
-vim.cmd([[
-    " autocmd FileType c,cpp,objc nnoremap <buffer><Leader>f :<C-u>ClangFormat<CR>
-    autocmd FileType c,cpp,objc nnoremap <buffer><Leader>f<Space> :<C-u>ClangFormat<CR>
-    autocmd FileType c,cpp,objc vnoremap <buffer><Leader>f<Space> :ClangFormat<CR>
-    let g:clang_format#code_style = 'google'
-    let g:clang_format#style_options = {
-                \ "AlignConsecutiveAssignments": "false",
-                \ "AllowShortFunctionsOnASingleLine": "Inline",
-                \ "BreakConstructorInitializers": "AfterColon",
-                \ "IndentWidth": 4,
-                \ "SpaceAfterTemplateKeyword": "false",
-                \ "TabWidth": 4,
-                \ "UseTab": "Never",
-                \ "SortIncludes": "false"}
-]])
+vim.api.nvim_create_autocmd('FileType', {
+    pattern = { 'c', 'cpp', 'objc' },
+    callback = function()
+        vim.api.nvim_buf_set_keymap(0, 'n', '<Leader>f<Space>', ':<C-u>ClangFormat<CR>', { noremap = true })
+        vim.api.nvim_buf_set_keymap(0, 'v', '<Leader>f<Space>', ':ClangFormat<CR>', { noremap = true })
+    end })
+
+vim.g['clang_format#code_style'] = 'google'
+vim.g['clang_format#style_options'] = {
+    AlignConsecutiveAssignments = 'false',
+    AllowShortFunctionsOnASingleLine = 'Inline',
+    BreakConstructorInitializers = 'AfterColon',
+    IndentWidth = 4,
+    SpaceAfterTemplateKeyword = 'false',
+    TabWidth = 4,
+    UseTab = 'Never',
+    SortIncludes = 'false'
+}
