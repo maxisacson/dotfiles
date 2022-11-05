fe956905f40cbeabb2d66f617a7c2d27ffd19cc1
Max Isacson <5937020+maxisacson@users.noreply.github.com>
2022-11-05 15:50:44 +0100

    comment out SmartTab

--- nvim/setup/custom.lua
+++ nvim/setup/custom.lua
@@ -28,19 +28,19 @@ vim.api.nvim_create_autocmd("WinLeave", { group = ag_clear, pattern = "*", comma
 
 vim.cmd([[
     " Use tab and shift-tab to move in quickfix and location lists
-    function! SmartTab(shifttab)
-        let isquickfix = !empty(filter(getwininfo(), "v:val.quickfix && !v:val.loclist"))
-        let isloclist = !empty(filter(getwininfo(), "v:val.loclist"))
-        if isquickfix
-            return a:shifttab ? ":cprev\<CR>" : ":cnext\<CR>"
-        elseif isloclist
-            return a:shifttab ? ":lprev\<CR>" : ":lnext\<CR>"
-        else
-            return a:shifttab ? "\<S-Tab>" : "\<Tab>"
-        endif
-    endfunction
-    nnoremap <silent> <expr> <Tab> SmartTab(0)
-    nnoremap <silent> <expr> <S-Tab> SmartTab(1)
+    " function! SmartTab(shifttab)
+    "     let isquickfix = !empty(filter(getwininfo(), "v:val.quickfix && !v:val.loclist"))
+    "     let isloclist = !empty(filter(getwininfo(), "v:val.loclist"))
+    "     if isquickfix
+    "         return a:shifttab ? ":cprev\<CR>" : ":cnext\<CR>"
+    "     elseif isloclist
+    "         return a:shifttab ? ":lprev\<CR>" : ":lnext\<CR>"
+    "     else
+    "         return a:shifttab ? "\<S-Tab>" : "\<Tab>"
+    "     endif
+    " endfunction
+    " nnoremap <silent> <expr> <Tab> SmartTab(0)
+    " nnoremap <silent> <expr> <S-Tab> SmartTab(1)
 
     " Function to insert include guards in cpp headers
     function! InsertCppIncludeGuard()
