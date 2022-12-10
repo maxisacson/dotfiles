3212619f20597552f578ca0db69d036a6f6e1c90
Max Isacson <5937020+maxisacson@users.noreply.github.com>
2022-12-10 18:07:47 +0100

    nvim gitsigns: update config

--- nvim/setup/gitsigns.lua
+++ nvim/setup/gitsigns.lua
@@ -1,12 +1,16 @@
-require('gitsigns').setup {
-    current_line_blame = true,
+local gitsigns = require('gitsigns')
+gitsigns.setup {
+    current_line_blame = false,
     current_line_blame_opts = {
         delay = 250,
     }
 }
 
 if vim.g.vimrc.colorscheme == 'gruvbox' then
-    vim.api.nvim_set_hl(0, 'GitSignsAdd', { link='GruvboxGreenSign' })
-    vim.api.nvim_set_hl(0, 'GitSignsChange', { link='GruvboxOrangeSign' })
-    vim.api.nvim_set_hl(0, 'GitSignsDelete', { link='GruvboxRedSign' })
+    vim.api.nvim_set_hl(0, 'GitSignsAdd', { link = 'GruvboxGreenSign' })
+    vim.api.nvim_set_hl(0, 'GitSignsChange', { link = 'GruvboxOrangeSign' })
+    vim.api.nvim_set_hl(0, 'GitSignsDelete', { link = 'GruvboxRedSign' })
 end
+
+vim.keymap.set('n', '<leader>gl', function() gitsigns.blame_line({ full = true }) end, { silent = true })
+vim.keymap.set('n', '<leader>gb', gitsigns.toggle_current_line_blame, { silent = true })
