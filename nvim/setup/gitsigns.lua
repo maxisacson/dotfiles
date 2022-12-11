14fb64435d6784ed13f03fb00361e601470d202e
Max Isacson <5937020+maxisacson@users.noreply.github.com>
2022-12-11 16:43:06 +0100

    nvim gitsigns: more keymaps

--- nvim/setup/gitsigns.lua
+++ nvim/setup/gitsigns.lua
@@ -1,9 +1,20 @@
 local gitsigns = require('gitsigns')
+
+local on_attach = function(bufnr)
+
+    vim.keymap.set('n', '<leader>gl', function() gitsigns.blame_line({ full = true }) end, { buffer = bufnr, silent = true, desc = "[GitSigns] Blame current line " })
+    vim.keymap.set('n', '<leader>gb', gitsigns.toggle_current_line_blame, { buffer = bufnr, silent = true, desc = "[GitSigns] Toggle line blame" })
+    vim.keymap.set('n', '<leader>gs', gitsigns.stage_hunk, { buffer = bufnr, silent = true, desc = "[GitSigns] Stage hunk under cursor" })
+    vim.keymap.set('n', '<leader>gu', gitsigns.undo_stage_hunk, { buffer = bufnr, silent = true, desc = "[GitSigns] Undo last stage hunk" })
+
+end
+
 gitsigns.setup {
     current_line_blame = false,
     current_line_blame_opts = {
         delay = 250,
-    }
+    },
+    on_attach = on_attach
 }
 
 if vim.g.vimrc.colorscheme == 'gruvbox' then
@@ -11,6 +22,3 @@ if vim.g.vimrc.colorscheme == 'gruvbox' then
     vim.api.nvim_set_hl(0, 'GitSignsChange', { link = 'GruvboxOrangeSign' })
     vim.api.nvim_set_hl(0, 'GitSignsDelete', { link = 'GruvboxRedSign' })
 end
-
-vim.keymap.set('n', '<leader>gl', function() gitsigns.blame_line({ full = true }) end, { silent = true })
-vim.keymap.set('n', '<leader>gb', gitsigns.toggle_current_line_blame, { silent = true })
