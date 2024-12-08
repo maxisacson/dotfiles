d5f1a3be7203ac4f823b1339b06f74fcc439071b
Max Isacson <5937020+maxisacson@users.noreply.github.com>
2024-12-08 19:50:09 +0100

    polybar: update charging animation framerate

--- utils/glyph.sh
+++ utils/glyph.sh
@@ -1,9 +1,11 @@
 #!/usr/bash
 
-fc-list ":charset=$1" | awk -F: '{print $2}' | sort | uniq | awk '{sub(/\s+/,""); printf "%-3d %s\n",NR,$0}'
+cp="$(echo -n "$1" | python -c "import sys;  print(f'{ord(sys.stdin.readline()):x}')")"
 
-eval "c=$'\U$1'"
-echo -e "\ncontaining glyph: $c (U+$1)"
+fc-list ":charset=$cp" | awk -F: '{print $2}' | sort | uniq | awk '{sub(/\s+/,""); printf "%-3d %s\n",NR,$0}'
+
+eval "c=$'\U$cp'"
+echo -e "\ncontaining glyph: $c (U+${cp})"
 
 # this is not reliable
 # font=$(FC_DEBUG=4 pango-view --font=monospace -t $c -q 2>&1 | grep family: | tail -n1 | awk 'match($0,/^\s*(family:)\s*"(.*)".*$/,m) {print m[2]}')
--- utils/styles.sh
+++ utils/styles.sh
@@ -1,5 +1,6 @@
 echo -e "\e[1mBold\e[0m"
 echo -e "\e[3mItalic\e[0m"
+echo -e "\e[1m\e[3mBoldItalic\e[0m"
 echo -e "\e[4mUnderline\e[0m"
 echo -e "\e[4:3mUndercurl\e[0m"
 echo -e "\e[9mStrikethrough\e[0m"
